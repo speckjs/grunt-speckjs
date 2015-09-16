478ef6c7587da01b78f79bccb394419f1b967eff
Greg RV <gregvarias@gmail.com>
2015-09-15 23:19:54 -0700

    Update grunt tasks 'build' and 'test' so that linting is included.

--- Gruntfile.js
+++ Gruntfile.js
@@ -67,11 +67,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-tape');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'speck', 'tape']);
-
-  // By default, lint and run all tests.
-  grunt.registerTask('default', ['eslint', 'test']);
+  grunt.registerTask('build', ['clean', 'speck', 'tape']);
+  grunt.registerTask('test', ['eslint', 'build']);
 
 };
